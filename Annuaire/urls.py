8ec9b76ae74f5f2c0829e159c5cc0ea4eab1c7b3
Lucas Tine-Beres <lucastineberes@gmail.com>
2013-07-10 13:55:29 +0800

    added some config

--- Annuaire/urls.py
+++ Annuaire/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 
+
+from Course.views import CourseListView, CourseDetailView
+
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^course/$', CourseListView.as_view(), name = "course_list_view"),
+	url(r'^course/(?P<pk>\d+)/', CourseDetailView.as_view(), name = "course_detail_view"),
+	url(r'^search/', include('haystack.urls')),
     # Examples:
     # url(r'^$', 'Annuaire.views.home', name='home'),
     # url(r'^Annuaire/', include('Annuaire.foo.urls')),
@@ -13,5 +19,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
--- Course/views.py
+++ Course/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from Course.models import Course
+
+class CourseListView(ListView):
+	model = Course
+
+class CourseDetailView(DetailView):
+	model = Course
