5275bbd61624849078434c326d25356d62955144
Lucas Tine-Beres <lucastineberes@gmail.com>
2013-08-02 17:04:00 +0800

    removed files

--- Course/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from Course.models import Course, Price, Slot, Place, Note
-
-admin.site.register(Course)
-admin.site.register(Price)
-admin.site.register(Slot)
-admin.site.register(Place)
-admin.site.register(Note)
--- Course/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class AddressForm(forms.Form):
-    address = forms.CharField()
-    city = forms.CharField()
-    radius = forms.IntegerField()
